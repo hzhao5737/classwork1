9c4eac21e4b942a23da43eee43a12e599adcb51b
student 6 <student 6@10.8.33.157>
2017-01-09 12:32:30 -0500

    stuff

--- src/gui/simon/SimonScreenHunter.java
+++ src/gui/simon/SimonScreenHunter.java
@@ -9,12 +9,14 @@ import gui.screens.ClickableScreen;
 
 public class SimonScreenHunter extends ClickableScreen implements Runnable {
 
-	//private ProgressInterface progress;
+	private ProgressInterface progress;
+	private ButtonInterfaceHunter[] button;
 	private ArrayList<MoveInterfaceHunter> moves;
+	private TextLabel label;
 	private int roundNumber;
 	private boolean isUserTurn;
-	private TextLabel roundLabel;
-	private TextLabel label;
+	private int sequenceIndex;
+	private int lastSelectedButton;
 	
 	public SimonScreenHunter(int width, int height) {
 		super(width, height);
@@ -29,11 +31,37 @@ public class SimonScreenHunter extends ClickableScreen implements Runnable {
 	}
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		addButtons();
+		progress = getProgress();
+		label = new TextLabel(130,230,300,40,"Let's play Simon!");
 		moves = new ArrayList<MoveInterfaceHunter>();
-		label = new TextLabel(350,220,150,40,"Simon's turn");
+		//add 2 moves to start
+		lastSelectedButton = -1;
+		moves.add(randomMove());
+		moves.add(randomMove());
+		roundNumber = 0;
+		viewObjects.add(progress);
 		viewObjects.add(label);
 	}
 	
+	private MoveInterfaceHunter randomMove() {
+		ButtonInterfaceHunter b;
+		//code that randomly selects a ButtonInterfaceX
+		return getMove(b);
+	}
+
+	private ProgressInterface getProgress() {
+		/**
+		Placeholder until partner finishes implementation of ProgressInterface
+		*/
+		return null;
+	}
+
+	private void addButtons() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private void changeText(String string) {
 		label.setText(string);
 		try {
