f028ad43b13bbbf359bab299f99c12a9e9fc2069
student 6 <student 6@10.8.33.151>
2017-05-11 12:31:30 -0400

    stuff

--- src/symposium/Background.java
+++ src/symposium/Background.java
@@ -1,10 +1,7 @@
 package symposium;
 
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.RenderingHints;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 
@@ -12,21 +9,21 @@ import gui.components.Component;
 
 public class Background extends Component{
 
-	private String background = "resources/sampleImages/Pallet.png";
 	private int file;
 
 	public Background(int x, int y, int w, int h, int file){
-		super(x, y, w, h);
+		super(x,y,w,h);
 		this.file = file;
 		update();
 	}
 
 	@Override
 	public void update(Graphics2D g) {
+		String[] background = {"resources/sampleImages/Pallet.png", "resources/sampleImages/Block.png"};
 		g = clear();
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		ImageIcon image = new ImageIcon(background);
-		g.drawImage(image.getImage(), 0, 0, 640, 480, null);
+		ImageIcon image = new ImageIcon(background[file]);
+		g.drawImage(image.getImage(), 0, 0, getWidth(), getHeight(), null);
 	}
 }
--- src/symposium/WorldMain.java
+++ src/symposium/WorldMain.java
@@ -5,22 +5,56 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import gui.Screen;
 import gui.components.Visible;
 
-public class WorldMain extends Screen {
+public class WorldMain extends Screen implements Runnable, KeyListener{
 	
-	private Background img;
+	private Background background;
+	private Background block;
+	private Background block1;
 
 	public WorldMain(int width, int height) {
 		super(width, height);
+		Thread play = new Thread(this);
+		play.start();
 	}
 
 	@Override
 	public void initObjects(ArrayList<Visible> v) {
-		img = new Background(0,0,640,480,0);
-		v.add(img);
+		background = new Background(0,0,640,480,0);
+		v.add(background);
+		block = new Background(64,456,32,24,1);
+		v.add(block);
+		block1 = new Background(96,456,32,24,1);
+		v.add(block1);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
